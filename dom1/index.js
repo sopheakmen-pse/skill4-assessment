dc0aa4def6c53e337a9de355c43927c42eb555b1
KhoeunSopheak <sopheak.khoeun@instile.pse.ngo>
2024-06-06 11:03:54 +0700

    submit

--- dom1/index.js
+++ dom1/index.js
@@ -2,3 +2,7 @@ const article = {
   title: "How to code",
   description: "You can learn to code. Error is nothing to be afraid of.",
 };
+
+function click(){
+document.getElementById("article").innerText ="hello";
+};
